27fd3f3ad3a0272ee7688d65e2c80ff7177b4994
crackerbad <anonymouscrackerbad@gmail.com>
2025-05-10 21:54:18 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -219,7 +219,6 @@ export class MicrosoftRewardsBot {
 
             // Login into MS Rewards, then go to rewards homepage
             await this.login.login(this.homePage, account.email, account.password)
-            this.accessToken = await this.login.getMobileAccessToken(this.homePage, account.email)
 
             await this.browser.func.goHome(this.homePage)
 
