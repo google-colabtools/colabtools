8aaa442bf9e92d1a78ad78bb467954b6a6060b36
crackerbad <anonymouscrackerbad@gmail.com>
2025-05-10 22:29:21 -0300

    Update Login.ts

--- src/functions/Login.ts
+++ src/functions/Login.ts
@@ -125,15 +125,7 @@ export class Login {
     private async enterEmail(page: Page, email: string) {
         const emailInputSelector = 'input[type="email"]'
     
-        try {
-            // Wait for email field
-            const emailField = await page.waitForSelector(emailInputSelector, { state: 'visible', timeout: 2000 }).catch(() => null)
-            if (!emailField) {
-                this.bot.log(this.bot.isMobile, 'LOGIN', 'Login error', 'warn')
-                // Agora lança uma exceção para forçar a voltar para login()
-                throw new Error('Login Error, restarting login')
-            }
-    
+        try {    
             await this.bot.utils.wait(1000)
     
             // Check if email is prefilled
@@ -299,6 +291,64 @@ export class Login {
         }
     }
 
+    async getMobileAccessToken(page: Page, email: string) {
+        const authorizeUrl = new URL(this.authBaseUrl)
+    
+        authorizeUrl.searchParams.append('response_type', 'code')
+        authorizeUrl.searchParams.append('client_id', this.clientId)
+        authorizeUrl.searchParams.append('redirect_uri', this.redirectUrl)
+        authorizeUrl.searchParams.append('scope', this.scope)
+        authorizeUrl.searchParams.append('state', crypto.randomBytes(16).toString('hex'))
+        authorizeUrl.searchParams.append('access_type', 'offline_access')
+        authorizeUrl.searchParams.append('login_hint', email)
+    
+        await page.goto(authorizeUrl.href)
+    
+        let currentUrl = new URL(page.url())
+        let code: string | null = null
+    
+        this.bot.log(this.bot.isMobile, 'LOGIN-APP', 'Waiting for authorization...')
+    
+        const timeout = 2 * 60 * 1000 // 2 minutos
+        const startTime = Date.now()
+    
+        while (true) {
+            if (currentUrl.hostname === 'login.live.com' && currentUrl.pathname === '/oauth20_desktop.srf') {
+                code = currentUrl.searchParams.get('code')
+                break
+            }
+    
+            if (Date.now() - startTime > timeout) {
+                this.bot.log(this.bot.isMobile, 'LOGIN-APP', 'Authorization timeout reached. Exiting...', 'error')
+                throw new Error('Authorization did not complete in time. Please try again.')
+            }
+    
+            currentUrl = new URL(page.url())
+            await this.bot.utils.wait(5000)
+        }
+    
+        const body = new URLSearchParams()
+        body.append('grant_type', 'authorization_code')
+        body.append('client_id', this.clientId)
+        body.append('code', code!)
+        body.append('redirect_uri', this.redirectUrl)
+    
+        const tokenRequest: AxiosRequestConfig = {
+            url: this.tokenUrl,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: body.toString()
+        }
+    
+        const tokenResponse = await this.bot.axios.request(tokenRequest)
+        const tokenData: OAuth = await tokenResponse.data
+    
+        this.bot.log(this.bot.isMobile, 'LOGIN-APP', 'Successfully authorized')
+        return tokenData.access_token
+    }
+    
     private async checkAccountLocked(page: Page) {
         await this.bot.utils.wait(2000)
         const isLocked = await page.waitForSelector('#serviceAbuseLandingTitle', { state: 'visible', timeout: 1000 }).then(() => true).catch(() => false)
