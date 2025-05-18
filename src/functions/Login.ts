a08292675d7be3af510e85eb55521e269082882b
crackerbad <anonymouscrackerbad@gmail.com>
2025-05-18 02:21:23 -0300

    Update Login.ts

--- src/functions/Login.ts
+++ src/functions/Login.ts
@@ -364,7 +364,14 @@ export class Login {
         try {
             this.bot.log(this.bot.isMobile, 'LOGIN-BING', 'Verifying Bing login')
             await page.goto('https://www.bing.com/fd/auth/signin?action=interactive&provider=windows_live_id&return_url=https%3A%2F%2Fwww.bing.com%2F')
-            await page.waitForLoadState('domcontentloaded').catch(() => { })
+            await page.waitForLoadState('load');
+            await this.bot.utils.wait(1000);
+            const acceptButton = await page.$('#bnp_btn_accept a');
+            if (acceptButton) {
+                await acceptButton.click();
+                this.bot.log(this.bot.isMobile, 'LOGIN', '"Accept" button from Bing Cookie Banner clicked successfully');
+                await this.bot.utils.wait(3000); // Espera após o clique
+            }
             const maxIterations = 5
 
             for (let iteration = 1; iteration <= maxIterations; iteration++) {
